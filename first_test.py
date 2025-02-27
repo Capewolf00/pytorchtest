affc6705c3be96e933cdc7c077ab058e9d7cc1a1
riccardo.capellupo <cric@cit.tum.de>
2025-02-28 00:26:21 +0100

    first test with tensors and shapes

--- first_test.py
+++ first_test.py
@@ -1,29 +1,11 @@
 # pylint: disable=import-error
-"""
-This module demonstrates basic operations with PyTorch tensors,
-including creation, basic arithmetic, matrix multiplication,
-and automatic differentiation.
-"""
 
 import torch
 
-# Create a tensor
-x = torch.tensor([1.0, 2.0, 3.0])
-print("Tensor x:", x)
+shape = (2, 3, 5)
+tensor = torch.rand(shape)
 
-# Perform a basic operation
-y = x + 2
-print("Tensor y (x + 2):", y)
-
-# Perform matrix multiplication
-a = torch.tensor([[1, 2], [3, 4]])
-b = torch.tensor([[5, 6], [7, 8]])
-c = torch.matmul(a, b)
-print("Matrix multiplication result:\n", c)
-
-# Use autograd for automatic differentiation
-x = torch.tensor([1.0, 2.0, 3.0], requires_grad=True)
-y = x * 2
-z = y.mean()
-z.backward()
-print("Gradient of x:", x.grad)
+# We move our tensor to the current accelerator if available
+if torch.accelerator.is_available():
+    tensor = tensor.to(torch.accelerator.current_accelerator())
+    print(tensor)
